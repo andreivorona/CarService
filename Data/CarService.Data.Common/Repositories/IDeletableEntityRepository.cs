26cfcacb3ef915a6a99c3aeda3c7effb59098de5
andreivorona <andreivorona@abv.bg>
2022-10-28 12:58:07 +0300

    Upload project files.

---
+++ Data/CarService.Data.Common/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace CarService.Data.Common.Repositories
+{
+    using System.Linq;
+
+    using CarService.Data.Common.Models;
+
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
